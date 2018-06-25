0a4a6bcea0eefb6e74577736f629f6c885c865b0
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-06-25 09:49:12 +0100

    Return subscription path
    To be used with connection string.

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "name" {
   value = "${azurerm_template_deployment.subscription.name}"
 }
+
+output "path" {
+  value = "${azurerm_template_deployment.subscription.parameters.serviceBusNamespaceName}/${azurerm_template_deployment.subscription.parameters.serviceBusTopicName}/${azurerm_template_deployment.subscription.parameters.serviceBusSubscriptionName}"
+}
