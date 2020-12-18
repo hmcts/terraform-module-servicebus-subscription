b69c6f2c9f50901c48dae7f97f39c39dac689f94
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-12-18 13:55:08 +0000

    Fixed var reference issue

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_servicebus_subscription" "servicebus_subscription" {
   lock_duration                     = var.lock_duration
   max_delivery_count                = var.max_delivery_count
   forward_to                        = var.forward_to
-  forward_dead_lettered_messages_to = forward_dead_lettered_messages_to
+  forward_dead_lettered_messages_to = var.forward_dead_lettered_messages_to
 
   requires_session                     = false
   dead_lettering_on_message_expiration = true
