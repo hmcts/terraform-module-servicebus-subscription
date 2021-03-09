65f054ee40d0fbf5ccec523da031ed86143ea761
Vijay Kumar Rajagopalan <62386423+vijayrajagopalan-hmcts@users.noreply.github.com>
2021-03-09 16:10:25 +0000

    Update main.tf
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "azurerm_servicebus_subscription" "servicebus_subscription" {
   forward_to                        = var.forward_to
   forward_dead_lettered_messages_to = var.forward_dead_lettered_messages_to
 
-  requires_session                     = false
+  requires_session                     = var.requires_session
   dead_lettering_on_message_expiration = true
   enable_batched_operations            = false
   default_message_ttl                  = "P10675199DT2H48M5.4775807S"
