d0713f639f35752f7cc221220b71ca1e3d64956a
msl8r <72858053+msl8r@users.noreply.github.com>
2022-04-20 15:12:33 +0100

    DTSPO-6371 - stop using deprecated topic_name

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
+data "azurerm_servicebus_topic" "this" {
+  name                = var.topic_name
+  resource_group_name = var.resource_group_name
+  namespace_name      = var.namespace_name
+}
+
 resource "azurerm_servicebus_subscription" "servicebus_subscription" {
   name                = var.name
   resource_group_name = var.resource_group_name
   namespace_name      = var.namespace_name
-  topic_name          = var.topic_name
+  topic_id            = data.azurerm_servicebus_topic.this.id
 
   lock_duration                     = var.lock_duration
   max_delivery_count                = var.max_delivery_count
